58019279b86040590c379f73b8074e83f04590ee
bitwana_fe <bitwana_fe@subnet.lv>
2025-03-04 12:01:39 +0200

    syncing

---
+++ TestNuget/Program.cs
@@ -0,0 +1,11 @@
+﻿using KrapivinHashTable;
+
+class Program
+{
+    static void Main()
+    {
+        var hashTable = new KrapivinHashTable<string, object>();
+        hashTable.Insert("key1", "value1");
+        Console.WriteLine(hashTable.Get("key1")); // Should print: value1
+    }
+}
